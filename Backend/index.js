dff9cc63fac0cf879ea4a43a79c994266ce24977
Sahiloddin <mohammdsahil3171@gmail.com>
2026-04-08 22:34:05 +0530

    fix cors and port

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,12 @@ const connectDb = require("./utils/db");
 
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}
+));
 
 app.use(express.json());
 
@@ -17,7 +22,7 @@ app.use("/api/auth/", router);
 app.use("/api/user/", userrouter);
 app.use("/api/user/student", studentrouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 connectDb().then(() => {
   app.listen(PORT, () => {
     console.log(`server is running at port: ${PORT}`);
